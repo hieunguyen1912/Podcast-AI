762a032bb406d536390a539582512e070ddad320
HieuNguyeen <hn151331@gmail.com>
2025-09-22 16:06:07 +0700

    first commit

---
+++ backend/src/main/java/com/hieunguyen/podcastai/PodcastaiApplication.java
@@ -0,0 +1,13 @@
+package com.hieunguyen.podcastai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastaiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hieunguyen/podcastai/PodcastaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieunguyen.podcastai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
